f62b2fa49896845016b8d143de81f08c9f104a30
xlf <2276974147@qq.com>
2024-09-10 01:30:13 +0800

    feat:添加一点我自己的东西

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div>我添加一个标题</div>
+    <button @click="count += 1">变更{{ count }}</button>
     <NuxtWelcome />
   </div>
 </template>
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
